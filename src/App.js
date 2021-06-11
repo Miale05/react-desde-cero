6e2f2391a2a8c0a14aec625453c5516310bc8dda
Miguel <miguel@Miguels-iMac.local>
2021-06-11 12:22:51 -0500

    Primer commit del curso React.js desde cero

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react";
+import "./styles/styles.scss";
+import Curso from './Curso'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <>
+    <div className="main-banner img-container l-section" id="main-banner">
+      <div className="ed-grid lg-grid-6">
+        <div className="lg-cols-4 lg-x-2">
+          <img
+            className="main-banner__img"
+            alt="banner"
+            src="https://i.pinimg.com/originals/45/4d/d3/454dd33dd8a85d430f1e50dcc82cee2d.jpg"
+          />
+          <div className="main-banner__data s-center">
+            <p className="t2 s-mb-0">Curso de React EDTeam</p>
+            <p>Tu futuro te esta esperando</p>
+            <a href="https://ed.team" className="button">
+              Suscribirse
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
-  );
-}
+
+    <div className="ed-grid m-grid-3">
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso></Curso>
+    </div>
+  </>
+);
 
 export default App;
+
+//REGLAS JSX
+//1: Toda etiqueta debe cerrarse
+//2: Los componentes deben devolver UN solo elemento padre
+//3: Apoyarse de los Fragment cuando necesito devover 2 elementos
+//4: Fragment => <> hijos </>
+//5: img siempre se cierra
+//6: class => className
+//7: for => htmlFor
